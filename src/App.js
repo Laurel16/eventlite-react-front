ccf4752e61404747ba3f1d027e8ff6b6ef6f1945
Laure Leter <laure.leter@gmail.com>
2020-09-08 10:19:24 +0200

    edit a record

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css'
 import AppHeader from './components/AppHeader'
 import Login from './components/Login'
 import Eventlite from './components/Eventlite'
+import Event from './components/Event'
 import Signup from './components/Signup'
+import EventForm from './components/EventForm'
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom"
 
 
@@ -24,6 +26,18 @@ function App() {
       <Route exact path="/">
         <Eventlite />
       </Route>
+      <Route exact path="/events/:id">
+        <Event/>
+      </Route>
+      <Route exact path="/events/:id" render={routeProps => (
+      <Event {...routeProps} />
+        )}/>
+      <Route exact path="/events/:id/edit" render={routeProps=>(
+        currentUser ?
+       <EventForm {...routeProps} /> :
+       <Redirect to='/login' />
+       )}/>
+
       <Route exact path="/login">
         {currentUser ? <Redirect to="/" /> : <Login />}
       </Route>
--- src/components/Eventlite.js
+++ src/components/Eventlite.js
@@ -3,8 +3,7 @@ import React from'react'
 import axios from 'axios'
 import EventsList from'./EventsList'
 import EventForm from'./EventForm'
-import FormErrors from'./FormErrors'
-import validations from'../validations'
+
 import './Eventlite.css'
 
 
@@ -12,12 +11,7 @@ class Eventlite extends React.Component{
   constructor(props) {
     super(props)
     this.state = {
-      events: [],
-      title: {value: '', valid: false},
-      start_datetime: {value: '', valid: false},
-      location: {value: '', valid: false},
-      formErrors: '',
-      formValid: false
+      events: []
     }
 
   }
@@ -33,122 +27,20 @@ componentDidMount() {
   }
 
 
-static formValidations = {
-  title: [
-      (value) => { return(validations.checkMinLength(value, 3)) }
-    ],
-    start_datetime: [
-      (value) => { return(validations.checkMinLength(value, 1)) },
-      (value) => { return(validations.timeShouldBeInTheFuture(value)) }
-    ],
-    location: [
-      (value) => { return(validations.checkMinLength(value, 1)) }
-    ]
-}
-
-  handleInput = e => {
-    e.preventDefault()
-    const name = e.target.name
-    const value = e.target.value
-    const newState = {}
-    newState[name] = {...this.state[name], value: value}
-    this.setState(newState, () => this.validateField(name, value, Eventlite.formValidations[name]))
-}
-
-  handleSubmit = e => {
-    e.preventDefault()
-    let newEvent = { title: this.state.title.value, start_datetime: this.state.start_datetime.value,location:this.state.location.value}
-    axios({
-      method: 'POST',
-      url: 'http://localhost:3001/events',
-      headers: JSON.parse(localStorage.user),
-      data: { event: newEvent }
-    })
-    .then(response => {
-      this.addNewEvent(response.data)
-      this.resetFormErrors();
-    })
-    .catch(error => {
-      console.log(error.response.data)
-      this.setState({formErrors: error.response.data})
-}) }
-
-validateForm(){
-let formErrors = {}
-let formValid = true
-if(this.state.title.value.length <= 2) {
-  formErrors.title = ["is too short (minimum is 3 characters)"]
-  formValid = false
-}
-if(this.state.location.value.length === 0) {
-formErrors.location = ["can't be blank"]
-formValid = false
-}
-
-if(this.state.start_datetime.value.length === 0) {
-formErrors.start_datetime = ["can't be blank"]
-formValid = false
-}
-
-else if(Date.parse(this.state.start_datetime.value) <= Date.now()) {
-formErrors.start_datetime = ["can't be in the past"]
-formValid = false
-}
-this.setState({formValid: this.state.title.valid && this.state.location.valid && this.state.start_datetime.valid})
- }
-
-resetFormErrors () {
-    this.setState({formErrors: {}})
-  }
-
-
-validateField(fieldName, fieldValue, fieldValidations) {
-    let fieldValid = true
-    let errors = fieldValidations.reduce((errors, validation) => {
-      let [valid, fieldError] = validation(fieldValue)
-      if(!valid) {
-        errors = errors.concat([fieldError])
-      }
-      return(errors);
-    }, []);
-
-    fieldValid = errors.length === 0
-
-    const newState = {formErrors: {...this.state.formErrors, [fieldName]: errors}}
-    newState[fieldName] = {...this.state[fieldName], valid: fieldValid}
-    this.setState(newState, this.validateForm)
-  }
-
-
-
   addNewEvent = (event) => {
     const events = [...this.state.events, event].sort(function(a, b){
 return new Date(a.start_datetime) - new Date(b.start_datetime) })
-    this.setState({events: events}, this.changeLogoColour)
+    this.setState({events: events})
   }
 
 render() {
   const currentUser = localStorage.getItem('user')
   return (
       <div>
-      <FormErrors formErrors = {this.state.formErrors} />
-      {currentUser &&
-        <EventForm handleSubmit = {this.handleSubmit}
-          handleInput = {this.handleInput}
-          formValid={this.state.formValid}
-          title = {this.state.title.value}
-          start_datetime = {this.state.start_datetime.value}
-          location = {this.state.location.value} />
-      }
+        {currentUser &&
+          <EventForm onSuccess={this.addNewEvent} />
+        }
         <EventsList events={this.state.events} />
-
       </div>
-  )
- }
-}
-
-
-
-
-
-export default Eventlite
+) }
+} export default Eventlite
